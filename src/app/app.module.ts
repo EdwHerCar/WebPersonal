230964a048703e0d0d1317bbbe37ea7eee565be5
edwardhercar <64983049+edwardhercar@users.noreply.github.com>
2020-12-04 15:49:48 -0600

    primera parte-navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortadaComponent } from './components/portada/portada.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    PortadaComponent,
+    AcercaDeComponent,
+    PortafolioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
